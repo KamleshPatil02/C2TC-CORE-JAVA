300986466f7a3f7c93cd72c2bff2e17414d4268f
patil <patil@MSI>
2022-11-26 15:03:21 +0530

    demo commit

---
+++ C2TC/src/com/cg/corejava/D13_collection/Student.java
@@ -0,0 +1,23 @@
+package com.cg.corejava.D13_collection;
+
+public class Student 
+{
+
+	private int rollNo;
+	private String name;
+	private float per;
+	public Student(int rollNo, String name, float per) {
+		super();
+		this.rollNo = rollNo;
+		this.name = name;
+		this.per = per;
+	}
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Student [rollNo=" + rollNo + ", name=" + name + ", per=" + per + "]";
+	}
+}
